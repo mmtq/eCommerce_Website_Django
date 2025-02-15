36565c226ff79ac16c1b8173202d5a6e5b780421
mmtq <mirtarhimul@gmail.com>
2025-02-15 14:10:52 +0600

    Categories/Search Implemented in shop page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from product.models import Product, Category
 # Create your views here.
 
@@ -12,8 +13,18 @@ def index(request):
 
 def shop(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
+    active_category = request.GET.get('category')
+    if active_category:
+        products = products.filter(category__slug=active_category)
+    
+    query = request.GET.get('query')
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
     
     context = {
-        'products': products
+        'products': products,
+        'categories': categories,
+        'active_category': active_category
     }
     return render(request, 'core/shop.html', context)
